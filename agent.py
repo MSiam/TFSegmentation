c388cc773a10b1839bf6f10d952fe8ad91da165c
Moemen <runmoemen@gmail.com>
2017-08-10 10:27:08 +0200

    init structure 6

---
+++ agent.py
@@ -0,0 +1,52 @@
+"""
+This class will take control of the whole process of training or testing Segmentation models
+"""
+
+from dirs import *
+from models import *
+from train import *
+from test import *
+import tensorflow as tf
+
+
+class Agent:
+    """
+    Agent will run the program
+    Choose the type of operation
+    Create a model
+    reset the graph and create a session
+    Create a trainer or tester
+    Then run it and handle it
+    """
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        print("Agent is running now...")
+        # TODO Select the Mode
+
+        # TODO Create Experiment Directory and the output directory and get hold of data dir
+
+        # Reset the graph
+        tf.reset_default_graph()
+
+        # Create the session of the graph with some configuration
+        # TODO Check that there is a running GPU
+        # pass the gpu fraction as a paramter
+        # gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=1)
+        # sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+
+        # TODO RUN the agent based on the type of the operation
+
+        # sess.close()
+        print("Agent is exited...")
+
+    def train(self):
+        pass
+
+    def test(self):
+        pass
+
+    def create_dirs(self):
+        pass
