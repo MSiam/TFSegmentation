547138717c7d76d3c22b62d79bf4300b7284966d
mgamal2 <mostafa.gamal.ext@valeo.com>
2017-08-10 13:29:34 +0200

    Convolution transpose updated

--- layers/convolution.py
+++ layers/convolution.py
@@ -94,10 +94,10 @@ def conv2d_transpose(name, x, output_shape, kernel_size=(3, 3), padding='SAME', 
 
 def conv2d_f(name, x, num_filters, kernel_size=(3, 3), padding='SAME', stride=(1, 1),
              initializer=tf.contrib.layers.xavier_initializer(), l2_strength=0.0, bias=0.0,
-             activation=tf.nn.relu, batchnorm_enabled=False, max_pool_enabled=True, dropout_keep_prob=1.0,
+             activation=None, batchnorm_enabled=False, max_pool_enabled=True, dropout_keep_prob=1.0,
              is_training=True):
     """
-    This block is responsible for a conv2d layer followed by optional (non-linearity, dropout, max-pooling).
+    This block is responsible for a convolution 2D layer followed by optional (non-linearity, dropout, max-pooling).
     Note that: "is_training" should be passed by a correct value based on being in either training or testing.
     :param name: (string) The name scope provided by the upper tf.name_scope('name') as scope.
     :param x: (tf.tensor) The input to the layer (N, H, W, C).
@@ -138,3 +138,51 @@ def conv2d_f(name, x, num_filters, kernel_size=(3, 3), padding='SAME', stride=(1
             conv_o = max_pool_2d_2x2(conv_o_dr)
 
     return conv_o
+
+
+def deconv2d_f(name, x, output_shape, kernel_size=(3, 3), padding='SAME', stride=(1, 1), l2_strength=0.0,
+               bias=0.0, activation=None, batchnorm_enabled=False, max_pool_enabled=True, dropout_keep_prob=1.0,
+               is_training=True):
+    """
+    This block is responsible for a convolution transpose 2D followed by optional (non-linearity, dropout, max-pooling).
+    Note that: "is_training" should be passed by a correct value based on being in either training or testing.
+    :param name: (string) The name scope provided by the upper tf.name_scope('name') as scope.
+    :param x: (tf.tensor) The input to the layer (N, H, W, C).
+    :param output_shape: (Array) [N, H', W', C'] The shape of the corresponding output.
+    :param kernel_size: (integer tuple) The size of the convolving kernel.
+    :param padding: (string) The amount of padding required.
+    :param stride: (integer tuple) The stride required.
+    :param l2_strength:(weight decay) (float) L2 regularization parameter.
+    :param bias: (float) Amount of bias.
+    :param activation: (tf.graph operator) The activation function applied after the convolution operation. If None, linear is applied.
+    :param batchnorm_enabled: (boolean) for enabling batch normalization.
+    :param max_pool_enabled:  (boolean) for enabling max-pooling 2x2 to decrease width and height by a factor of 2.
+    :param dropout_keep_prob: (float) for the probability of keeping neurons.
+    :param is_training: (boolean) to diff. between training and testing (important for batch normalization and dropout) 
+    :return out: The output of the layer. (output_shape[0], output_shape[1], output_shape[2], output_shape[3])
+    """
+    with tf.name_scope(name) as scope:
+        conv_o_b = conv2d_transpose(name=scope, x=x, output_shape=output_shape, kernel_size=kernel_size,
+                                    padding=padding, stride=stride,
+                                    l2_strength=l2_strength,
+                                    bias=bias)
+
+        if batchnorm_enabled:
+            conv_o_bn = tf.layers.batch_normalization(conv_o_b, training=is_training)
+            if not activation:
+                conv_a = conv_o_bn
+            else:
+                conv_a = activation(conv_o_bn)
+        else:
+            if not activation:
+                conv_a = conv_o_b
+            else:
+                conv_a = activation(conv_o_b)
+
+        conv_o_dr = tf.nn.dropout(conv_a, dropout_keep_prob)
+
+        conv_o = conv_o_dr
+        if max_pool_enabled:
+            conv_o = max_pool_2d_2x2(conv_o_dr)
+
+        return conv_o
