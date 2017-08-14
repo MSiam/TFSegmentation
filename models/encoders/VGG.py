d988c27b1882c7c567c11e3203f919a298cdd170
Mo'men AbdelRazek <runmoemen@gmail.com>
2017-08-14 23:01:47 +0200

    TODO revise VGG, FCN8s, Convolution

--- models/encoders/VGG.py
+++ models/encoders/VGG.py
@@ -1,121 +1,159 @@
 from layers.convolution import conv2d_f_pre, conv2d_f
+import numpy as np
 import tensorflow as tf
 
-class VGG16(object):
-    '''
+
+class VGG16:
+    """
     VGG 16 Encoder class
-    '''
+    """
 
     VGG_MEAN = [103.939, 116.779, 123.68]
 
-    def __init__(self, pretrained_path, reduced= False):
-        '''
-        reduced: whether to use vgg reduced with 512 channel for last two layers, or full with 4096 channels
-        pretrained_path: path to pretrained weights for vgg16 in npy format
-        '''
+    def __init__(self, x_input,
+                 num_classes,
+                 pretrained_path,
+                 train_flag,
+                 reduced_flag=False,
+                 weight_decay=5e-4):
+        """
+
+        :param x_input: Input to the VGG Encoder
+        :param num_classes:
+        :param pretrained_path: Path of the numpy which contain the weights
+        :param train_flag: Flag of training or inference
+        :param reduced_flag:
+        :param weight_decay:
+        """
+
         # Load pretrained path
         self.pretrained_weights = np.load(pretrained_path, encoding='latin1').item()
         print('pretrained weights loaded')
 
-        self.reduced_flag= reduced
-        self.wd = 5e-4
+        # init parameters and input
+        self.x_input = x_input
+        self.num_classes = num_classes
+        self.train_flag = train_flag
+        self.reduced_flag = reduced_flag
+        self.wd = weight_decay
 
-    def build(self, img_input, train=False, num_classes=20):
+        # All layers
+        self.conv1_1 = None
+        self.conv1_2 = None
+
+        self.conv2_1 = None
+        self.conv2_2 = None
+
+        self.conv3_1 = None
+        self.conv3_2 = None
+        self.conv3_3 = None
+
+        self.conv4_1 = None
+        self.conv4_2 = None
+        self.conv4_3 = None
+
+        self.conv5_1 = None
+        self.conv5_2 = None
+        self.conv5_3 = None
+
+        self.fc6 = None
+        self.fc7 = None
+        self.score_fr = None
+
+        # These feed layers are for the decoder
+        self.feed1 = None
+        self.feed2 = None
+
+    def build(self, ):
         """
         Build the VGG model using loaded weights
         Parameters
         ----------
-        img_input: image batch tensor
-            Image in rgb shap. Scaled to Intervall [0, 255]
-        train: bool
+        x_input: image batch tensor
+            Image in rgb shape. Scaled to Interval [0, 255]
+        train_flag_tensor: bool
             Whether to build train or inference graph
         num_classes: int
             How many classes should be predicted (by fc8)
         """
-        # Convert RGB to BGR
 
+        # Convert RGB to BGR
         with tf.name_scope('Processing'):
-            red, green, blue = tf.split(img_input, 3, 3)
-            # assert red.get_shape().as_list()[1:] == [224, 224, 1]
-            # assert green.get_shape().as_list()[1:] == [224, 224, 1]
-            # assert blue.get_shape().as_list()[1:] == [224, 224, 1]
-            img_input = tf.concat([
+            red, green, blue = tf.split(self.x_input, num_or_size_splits=3, axis=3)
+            preprocessed_input = tf.concat([
                 blue - VGG16.VGG_MEAN[0],
                 green - VGG16.VGG_MEAN[1],
                 red - VGG16.VGG_MEAN[2],
             ], 3)
 
-        self.conv1_1= load_conv_layer(img_input, 'conv1_1')
-        self.conv1_2= load_conv_layer(self.conv1_1, 'conv1_2', pooling=True)
+        self.conv1_1 = self.load_conv_layer(preprocessed_input, 'conv1_1')
+        self.conv1_2 = self.load_conv_layer(self.conv1_1, 'conv1_2', pooling=True)
 
-        self.conv2_1= load_conv_layer(self.conv1_2, 'conv2_1')
-        self.conv2_2= load_conv_layer(self.conv2_1, 'conv2_2', pooling=True)
+        self.conv2_1 = self.load_conv_layer(self.conv1_2, 'conv2_1')
+        self.conv2_2 = self.load_conv_layer(self.conv2_1, 'conv2_2', pooling=True)
 
-        self.conv3_1= load_conv_layer(self.conv2_2, 'conv3_1')
-        self.conv3_2= load_conv_layer(self.conv3_1, 'conv3_2')
-        self.conv3_3= load_conv_layer(self.conv3_2, 'conv3_3', pooling=True)
+        self.conv3_1 = self.load_conv_layer(self.conv2_2, 'conv3_1')
+        self.conv3_2 = self.load_conv_layer(self.conv3_1, 'conv3_2')
+        self.conv3_3 = self.load_conv_layer(self.conv3_2, 'conv3_3', pooling=True)
 
-        self.conv4_1= load_conv_layer(self.conv3_3, 'conv4_1')
-        self.conv4_2= load_conv_layer(self.conv4_1, 'conv4_2')
-        self.conv4_3= load_conv_layer(self.conv4_2, 'conv4_3', pooling=True)
+        self.conv4_1 = self.load_conv_layer(self.conv3_3, 'conv4_1')
+        self.conv4_2 = self.load_conv_layer(self.conv4_1, 'conv4_2')
+        self.conv4_3 = self.load_conv_layer(self.conv4_2, 'conv4_3', pooling=True)
 
-        self.conv5_1= load_conv_layer(self.conv4_3, 'conv5_1')
-        self.conv5_2= load_conv_layer(self.conv5_1, 'conv5_2')
-        self.conv5_3= load_conv_layer(self.conv5_2, 'conv5_3', pooling=True)
+        self.conv5_1 = self.load_conv_layer(self.conv4_3, 'conv5_1')
+        self.conv5_2 = self.load_conv_layer(self.conv5_1, 'conv5_2')
+        self.conv5_3 = self.load_conv_layer(self.conv5_2, 'conv5_3', pooling=True)
 
-        self.fc6= load_fc_layer(self.conv5_3, 'fc6', activation= tf.nn.relu, dropout=0.5, train=train)
-        self.fc7= load_fc_layer(self.fc6, 'fc7', activation= tf.nn.relu, dropout=0.5, train=train)
-        self.score_fr= load_fc_layer(self.fc7, 'score_fr', num_classes= num_classes)
+        self.fc6 = self.load_fc_layer(self.conv5_3, 'fc6', activation=tf.nn.relu, dropout=0.5, train=self.train_flag)
+        self.fc7 = self.load_fc_layer(self.fc6, 'fc7', activation=tf.nn.relu, dropout=0.5, train=self.train_flag)
+        self.score_fr = self.load_fc_layer(self.fc7, 'score_fr', num_classes=num_classes)
 
-        self.feed1= self.conv4_3
-        self.feed2= self.conv3_3
+        self.feed1 = self.conv4_3
+        self.feed2 = self.conv3_3
 
-    def load_fc_layer(self, bottom, name, num_classes=20,
-            activation= None, dropout= 1.0, train= False):
-        '''
+    def load_fc_layer(self, bottom, name, num_classes=20, activation=None, dropout=1.0, train=False):
+        """
         Load fully connected layers from pretrained weights in case of full vgg
         in case of reduced vgg initialize randomly
-        '''
-        if not self.reduced:
+        """
+        if not self.reduced_flag:
             if name == 'fc6':
                 w = self.get_fc_weight_reshape(name, [7, 7, 512, 4096])
             elif name == 'score_fr':
-                name= 'fc8'
-                w = self.get_fc_weight_reshape(name, [1, 1, 4096, 1000],
-                                                  num_classes=num_classes)
+                name = 'fc8'
+                w = self.get_fc_weight_reshape(name, [1, 1, 4096, 1000], num_classes=num_classes)
             else:
                 w = self.get_fc_weight_reshape(name, [1, 1, 4096, 4096])
 
             biases = self.get_bias(name, num_classes=num_classes)
-            return conv2d_f_pre(name, bottom, w, l2_strength= self.wd, bias=biases,
-                    activation=activation, dropout_keep_prob= dropout, is_training= train)
+            return conv2d_f_pre(name, bottom, w, l2_strength=self.wd, bias=biases,
+                                activation=activation, dropout_keep_prob=dropout, is_training=train)
         else:
             if name == 'fc6':
-                num_channels= 512
-                kernel_size= (7,7)
+                num_channels = 512
+                kernel_size = (7, 7)
             elif name == 'score_fr':
-                name= 'fc8'
-                num_channels= num_classes
-                kernel_size= (1,1)
+                name = 'fc8'
+                num_channels = num_classes
+                kernel_size = (1, 1)
             else:
-                num_channels= 512
-                kernel_size= (1,1)
+                num_channels = 512
+                kernel_size = (1, 1)
 
-            return conv2d_f(name, bottom, num_channel, kernel_size,= kernel_size, l2_strength=self.wd,
-                   activation=activation, dropout_keep_prob=dropout,is_training=train)
+            return conv2d_f(name, bottom, num_channel, kernel_size=kernel_size, l2_strength=self.wd, activation=activation, dropout_keep_prob=dropout, is_training=train)
 
-
-    def load_conv_layer(self,bottom, name, pooling= False):
+    def load_conv_layer(self, bottom, name, pooling=False):
         w = self.get_conv_filter(name)
         biases = self.get_bias(name)
-        return conv2d_f_pre(name, bottom, w, l2_strength= self.wd, bias=biases,
-                activation=tf.nn.relu, max_pool_enabled=pooling)
+        return conv2d_f_pre(name, bottom, w, l2_strength=self.wd, bias=biases,
+                            activation=tf.nn.relu, max_pool_enabled=pooling)
 
-    '''
+    """
     ==============================================================
     Next Functions are helpers for loading pretrained weights
     ==============================================================
-    '''
+    """
+
     def get_conv_filter(self, name):
         init = tf.constant_initializer(value=self.pretrained_weights[name][0],
                                        dtype=tf.float32)
@@ -154,5 +192,3 @@ class VGG16(object):
                                        dtype=tf.float32)
         var = tf.get_variable(name="weights", initializer=init, shape=shape)
         return var
-
-
