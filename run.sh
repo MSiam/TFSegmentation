84e4c8ccdb5e48f798828c7dfa6421c492bf4637
Mennatullah Siam <mennatul@ualberta.ca>
2017-08-27 19:07:29 -0600

    fixed bug in accumulated acc and loss

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 #python3 main.py --load_config=test.yaml overfit Train FCN8s
 
 # FCN8s exp test run
-python main.py --load_config=fcn8s_exp_test.yaml train Train FCN8s
+python main.py --load_config=test.yaml train Train FCN8s
