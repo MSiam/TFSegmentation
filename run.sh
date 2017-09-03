aa34423703beda70543444b1f88845b447f1cd30
Moemen ABDELRAZEK <runmoemen@gmail.com>
2017-09-04 00:10:10 +0200

    check time of initializing

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 #python3 main.py --load_config=test.yaml overfit Train FCN8s
 
 # FCN8s exp test run
-#python main.py --load_config=fcn8s_exp_test.yaml test Train FCN8s
-python main.py --load_config=fcn8s_exp_video.yaml test Train FCN8s
+python main.py --load_config=fcn8s_exp_test.yaml test Train FCN8s
+#python main.py --load_config=fcn8s_exp_video.yaml test Train FCN8s
