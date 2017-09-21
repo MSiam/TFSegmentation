d6b89fb467cae2de0ef9f51c30cc7154690650e2
mrgemy95 <mahmoud.gemy18@gmail.com>
2017-09-21 18:14:29 +0200

    init onavos

--- models/onavos.py
+++ models/onavos.py
@@ -11,7 +11,7 @@ class Onavos(BasicModel):
         pass
 
     def init_input(self):
-        self.img_placeholder = tf.placeholder(tf.float32, shape=(None, None, 3), name="img_placeholder")
-        self.label_placeholder = tf.placeholder(tf.uint8, shape=(None, None, 1), name="label_placeholder")
-        # self.tag_placeholder = tf.placeholder(tf.string, shape=(), name="tag_placeholder")
+        # self.img_placeholder = tf.placeholder(tf.float32, shape=(None, None, 3), name="img_placeholder")
+        # self.label_placeholder = tf.placeholder(tf.uint8, shape=(None, None, 1), name="label_placeholder")
+        # # self.tag_placeholder = tf.placeholder(tf.string, shape=(), name="tag_placeholder")
 
