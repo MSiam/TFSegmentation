f6cba4b4fad1654027dfb6e19cba925d65af1ded
Moemen ABDELRAZEK <runmoemen@gmail.com>
2017-09-23 23:50:18 +0200

    adding variable scope in deconv

--- models/linknet.py
+++ models/linknet.py
@@ -57,16 +57,16 @@ class LinkNET(BasicModel):
         self.out_decoder_block_1 = self._decoder_block('decoder_block_1', self.out_decoder_block_2, 64)
 
         with tf.variable_scope('output_block'):
-            self.out_full_conv1 = self._deconv(self.out_decoder_block_1, 32, (3, 3))
-            print("output_block_full_conv1: %s".format(str(self.out_full_conv1.shape.as_list())))
+            self.out_full_conv1 = self._deconv('deconv_out_1', self.out_decoder_block_1, 32, (3, 3))
+            print("output_block_full_conv1: %s" % (str(self.out_full_conv1.shape.as_list())))
             self.out_conv1 = tf.layers.conv2d(self.out_full_conv1, filters=32, kernel_size=(3, 3), padding="same",
                                               use_bias=False,
                                               kernel_initializer=tf.contrib.layers.xavier_initializer(),
                                               kernel_regularizer=tf.contrib.layers.l2_regularizer(
                                                   self.args.weight_decay))
-            print("output_block_conv1: %s".format(str(self.out_conv1.shape.as_list())))
-            self.fscore = self._deconv(self.out_conv1, self.params.num_classes, (2, 2))
-            print("logits: %s".format(str(self.fscore.shape.as_list())))
+            print("output_block_conv1: %s" % (str(self.out_conv1.shape.as_list())))
+            self.fscore = self._deconv('deconv_out_2', self.out_conv1, self.params.num_classes, (2, 2))
+            print("logits: %s" % (str(self.fscore.shape.as_list())))
 
         self.logits = self.fscore
 
@@ -77,7 +77,7 @@ class LinkNET(BasicModel):
             out = self._conv_1x1_block('conv_1', x, in_channels // 4)
             out = self._full_conv_3x3_block('deconv', out, in_channels // 4)
             out = self._conv_1x1_block('conv_2', out, out_channels)
-        print("Decoder block - %s: %s".format(name, str(out.shape.as_list())))
+        print("Decoder block - %s: %s" % (name, str(out.shape.as_list())))
         return out
 
     def _conv_1x1_block(self, name, x, filters):
@@ -91,18 +91,19 @@ class LinkNET(BasicModel):
 
     def _full_conv_3x3_block(self, name, x, out_channels):
         with tf.variable_scope(name):
-            out = self._deconv(x, out_channels, kernel_size=(3, 3))
+            out = self._deconv('deconv', x, out_channels, kernel_size=(3, 3))
             out = tf.layers.batch_normalization(out, training=self.is_training, fused=True)
             out = tf.nn.relu(out)
         return out
 
-    def _deconv(self, x, out_channels, kernel_size=(3, 3)):
-        h, w = x.shape.as_list()[1:3]
-        h, w = h * 2, w * 2
-        output_shape = [self.args.batch_size, h, w, out_channels]
-        stride = [1, 2, 2, 1]
-        kernel_shape = [kernel_size[0], kernel_size[1], out_channels, x.shape.as_list()[-1]]
-        w = get_deconv_filter(kernel_shape, self.args.weight_decay)
-        variable_summaries(w)
-        out = tf.nn.conv2d_transpose(x, w, tf.stack(output_shape), strides=stride, padding="SAME")
+    def _deconv(self, name, x, out_channels, kernel_size=(3, 3)):
+        with tf.variable_scope(name):
+            h, w = x.shape.as_list()[1:3]
+            h, w = h * 2, w * 2
+            output_shape = [self.args.batch_size, h, w, out_channels]
+            stride = [1, 2, 2, 1]
+            kernel_shape = [kernel_size[0], kernel_size[1], out_channels, x.shape.as_list()[-1]]
+            w = get_deconv_filter(kernel_shape, self.args.weight_decay)
+            variable_summaries(w)
+            out = tf.nn.conv2d_transpose(x, w, tf.stack(output_shape), strides=stride, padding="SAME")
         return out
