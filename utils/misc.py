726fe42dc4f25f6b20d6472abfa03ac253e7f650
Moemen ABDELRAZEK <runmoemen@gmail.com>
2017-09-23 20:05:48 +0200

    refactor get_class_weights function

--- utils/misc.py
+++ utils/misc.py
@@ -2,23 +2,31 @@ import time
 import pickle
 import numpy as np
 
-def get_weights(nclasses, npy_file):
-    yy= np.load(npy_file)
-    label_to_frequency= {}
-    for c in range(nclasses):
-        class_mask= np.equal(yy, c)
-        class_mask= class_mask.astype(np.float32)
-        label_to_frequency[c]= np.sum(class_mask)
 
-    #perform the weighing function label-wise and append the label's class weights to class_weights
+def get_class_weights(nclasses, npy_file):
+    """
+    This function get the weights of every class from labels to use it in the loss while training
+    :param nclasses: Number of classes of labels
+    :param npy_file: the numpy file of the training ex: Y_train.npy
+    :return: class_weights: which is a numpy array contain the weights of all classes
+    """
+    yy = np.load(npy_file)
+    label_to_frequency = {}
+    for c in range(nclasses):
+        class_mask = np.equal(yy, c)
+        class_mask = class_mask.astype(np.float32)
+        label_to_frequency[c] = np.sum(class_mask)
+
+    # perform the weighing function label-wise and append the label's class weights to class_weights
     class_weights = []
     total_frequency = sum(label_to_frequency.values())
     for label, frequency in label_to_frequency.items():
         class_weight = 1 / np.log(1.02 + (frequency / total_frequency))
         class_weights.append(class_weight)
 
-    class_weights[-1]=0
+    class_weights[-1] = 0
     return class_weights
+
 
 def timeit(f):
     """ Decorator to time Any Function """
