287b2bfe28871d51f2d55c4637f25599b8742a05
Moemen ABDELRAZEK <runmoemen@gmail.com>
2017-09-23 19:03:21 +0200

    prepare the reporter class

---
+++ utils/reporter.py
@@ -0,0 +1,10 @@
+"""
+This File will contain a reporter class which will allow us to report the whole experiment.
+"""
+
+
+class Reporter:
+    """
+    This class will contain APIs to facilitate the process of reporting the experiment
+    """
+    pass
