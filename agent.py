a86a5103ed1c547f8c47f22a01fc517fa4471a5a
Moemen ABDELRAZEK <runmoemen@gmail.com>
2017-09-24 02:07:55 +0200

    ready for a test run with batch size 4 change cuda visible device 0

--- agent.py
+++ agent.py
@@ -12,7 +12,7 @@ from utils.misc import timeit
 import os
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 
 class Agent:
