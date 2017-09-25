76cf82a571096482c8551cc620a92529a2b8dd30
mennatullah siam <mennatul@ualberta.ca>
2017-09-24 23:04:18 -0600

    changed unet mobilenet to linknet similar arch

--- data/preprocess_full.sh
+++ data/preprocess_full.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python preprocess_cityscapes.py --root=../../cityscapes/ --rescale=0.5 --output_file=train.h5 --mode=normal --bs=5
+python preprocess_cityscapes.py --root=/home/eren/Data/Cityscapes/ --rescale=0.5 --output_file=train.h5 --mode=normal --bs=5
--- models/unet_mobilenet.py
+++ models/unet_mobilenet.py
@@ -40,71 +40,66 @@ class UNetMobileNet(BasicModel):
 
         # Build Decoding part
         with tf.name_scope('upscale_1'):
-            self.upscale1 = conv2d_transpose('upscale1', x=self.encoder.conv5_6,
-                                             output_shape=self.encoder.conv5_5.shape.as_list()[0:3] + [
-                                                 self.encoder.conv5_6.shape.as_list()[3]], batchnorm_enabled=True,
-                                             kernel_size=(4, 4), stride=(2, 2), l2_strength=self.encoder.wd)
-            self._debug(self.upscale1)
-            #self.concat1 = tf.concat([self.upscale1, self.encoder.conv5_5], 3)
-            self.concat1 = tf.add([self.upscale1, self.encoder.conv5_5])
-            self._debug(self.concat1)
-            self.expand11 = conv2d('expand1_1', x=self.concat1, batchnorm_enabled=True,
-                                      num_filters=self.encoder.conv5_5.shape.as_list()[3], kernel_size=(3, 3),
+            self.expand11 = conv2d('expand1_1', x=self.encoder.conv5_6, batchnorm_enabled=True,
+                                      num_filters=self.encoder.conv5_5.shape.as_list()[3], kernel_size=(1, 1),
                                       l2_strength=self.encoder.wd)
             self._debug(self.expand11)
-            self.expand12 = conv2d('expand1_2', x=self.expand11, batchnorm_enabled=True,
-                                      num_filters=self.encoder.conv5_5.shape.as_list()[3], kernel_size=(3, 3),
+            self.upscale1 = conv2d_transpose('upscale1', x=self.expand11,
+                                             output_shape=self.encoder.conv5_5.shape.as_list(), batchnorm_enabled=True,
+                                             kernel_size=(4, 4), stride=(2, 2), l2_strength=self.encoder.wd)
+            self._debug(self.upscale1)
+            self.add1 = tf.add(self.upscale1, self.encoder.conv5_5)
+            self._debug(self.add1)
+            self.expand12 = conv2d('expand1_2', x=self.add1, batchnorm_enabled=True,
+                                      num_filters=self.encoder.conv5_5.shape.as_list()[3], kernel_size=(1, 1),
                                       l2_strength=self.encoder.wd)
             self._debug(self.expand12)
+
         with tf.name_scope('upscale_2'):
-            self.upscale2 = conv2d_transpose('upscale2', x=self.expand12,
-                                             output_shape=self.encoder.conv4_1.shape.as_list()[0:3] + [
-                                                 self.encoder.conv5_5.shape.as_list()[3]],
-                                             kernel_size=(4, 4), stride=(2, 2), l2_strength=self.encoder.wd)
-            self._debug(self.upscale2)
-            #self.concat2 = tf.concat([self.upscale2, self.encoder.conv4_1], 3)
-            self.concat2 = tf.add([self.upscale2, self.encoder.conv4_1])
-            self._debug(self.concat2)
-            self.expand21 = conv2d('expand2_1', x=self.concat2, batchnorm_enabled=True,
-                                      num_filters=self.encoder.conv4_1.shape.as_list()[3], kernel_size=(3, 3),
+            self.expand21 = conv2d('expand2_1', x=self.expand12, batchnorm_enabled=True,
+                                      num_filters=self.encoder.conv4_1.shape.as_list()[3], kernel_size=(1, 1),
                                       l2_strength=self.encoder.wd)
             self._debug(self.expand21)
-            self.expand22 = conv2d('expand2_2', x=self.expand21, batchnorm_enabled=True,
-                                      num_filters=self.encoder.conv4_1.shape.as_list()[3], kernel_size=(3, 3),
-                                      l2_strength=self.encoder.wd)
-            self._debug(self.expand22)
-        with tf.name_scope('upscale_3'):
-            self.upscale3 = conv2d_transpose('upscale3', x=self.expand22, batchnorm_enabled=True,
-                                             output_shape=self.encoder.conv3_1.shape.as_list()[0:3] + [
-                                                 self.encoder.conv4_1.shape.as_list()[3]],
+            self.upscale2 = conv2d_transpose('upscale2', x=self.expand21,
+                                             output_shape=self.encoder.conv4_1.shape.as_list(),
                                              kernel_size=(4, 4), stride=(2, 2), l2_strength=self.encoder.wd)
-            self._debug(self.upscale3)
-            #self.concat3 = tf.concat([self.upscale3, self.encoder.conv3_1], 3)
-            self.concat = tf.add([self.upscale3, self.encoder.conv3_1])
-            self._debug(self.concat3)
-            self.expand31 = conv2d('expand3_1', x=self.concat3, batchnorm_enabled=True,
-                                      num_filters=self.encoder.conv3_1.shape.as_list()[3], kernel_size=(3, 3),
+            self._debug(self.upscale2)
+            self.add2 = tf.add(self.upscale2, self.encoder.conv4_1)
+            self._debug(self.add2)
+            self.expand22 = conv2d('expand2_2', x=self.add2, batchnorm_enabled=True,
+                              num_filters=self.encoder.conv4_1.shape.as_list()[3], kernel_size=(1, 1),
+                              l2_strength=self.encoder.wd)
+            self._debug(self.expand22)
+
+        with tf.name_scope('upscale_3'):
+            self.expand31 = conv2d('expand3_1', x=self.expand22, batchnorm_enabled=True,
+                                      num_filters=self.encoder.conv3_1.shape.as_list()[3], kernel_size=(1, 1),
                                       l2_strength=self.encoder.wd)
             self._debug(self.expand31)
-            self.expand32 = conv2d('expand3_2', x=self.expand31, batchnorm_enabled=True,
-                                      num_filters=self.encoder.conv3_1.shape.as_list()[3], kernel_size=(3, 3),
-                                      l2_strength=self.encoder.wd)
-            self._debug(self.expand32)
-        with tf.name_scope('upscale_4'):
-            self.upscale4 = conv2d_transpose('upscale4', x=self.expand32, batchnorm_enabled=True,
-                                             output_shape=self.encoder.conv2_1.shape.as_list()[0:3] + [
-                                                 self.encoder.conv3_1.shape.as_list()[3]],
+            self.upscale3 = conv2d_transpose('upscale3', x=self.expand31, batchnorm_enabled=True,
+                                             output_shape=self.encoder.conv3_1.shape.as_list(),
                                              kernel_size=(4, 4), stride=(2, 2), l2_strength=self.encoder.wd)
-            self._debug(self.upscale4)
-            #self.concat4 = tf.concat([self.upscale4, self.encoder.conv2_1], 3)
-            self.concat4 = tf.add([self.upscale4, self.encoder.conv2_1])
-            self._debug(self.concat4)
-            self.expand41 = conv2d('expand4_1', x=self.concat4, batchnorm_enabled=True,
-                                      num_filters=self.encoder.conv2_1.shape.as_list()[3], kernel_size=(3, 3),
+            self._debug(self.upscale3)
+            self.add3 = tf.add(self.upscale3, self.encoder.conv3_1)
+            self._debug(self.add3)
+            self.expand32 = conv2d('expand3_2', x=self.add3, batchnorm_enabled=True,
+                                num_filters=self.encoder.conv3_1.shape.as_list()[3], kernel_size=(1, 1),
+                                l2_strength=self.encoder.wd)
+            self._debug(self.expand32)
+
+        with tf.name_scope('upscale_4'):
+            self.expand41 = conv2d('expand4_1', x=self.expand32, batchnorm_enabled=True,
+                                      num_filters=self.encoder.conv2_1.shape.as_list()[3], kernel_size=(1, 1),
                                       l2_strength=self.encoder.wd)
             self._debug(self.expand41)
-            self.expand42 = conv2d('expand4_2', x=self.expand41, batchnorm_enabled=True,
-                                      num_filters=self.encoder.conv2_1.shape.as_list()[3], kernel_size=(3, 3),
+            self.upscale4 = conv2d_transpose('upscale4', x=self.expand41, batchnorm_enabled=True,
+                                             output_shape=self.encoder.conv2_1.shape.as_list(),
+                                             kernel_size=(4, 4), stride=(2, 2), l2_strength=self.encoder.wd)
+            self._debug(self.upscale4)
+            self.add4 = tf.add(self.upscale4, self.encoder.conv2_1)
+            self._debug(self.add4)
+            self.expand42 = conv2d('expand4_2', x=self.add4, batchnorm_enabled=True,
+                                      num_filters=self.encoder.conv2_1.shape.as_list()[3], kernel_size=(1, 1),
                                       l2_strength=self.encoder.wd)
             self._debug(self.expand42)
 
@@ -114,9 +109,13 @@ class UNetMobileNet(BasicModel):
                                                  self.encoder.conv2_1.shape.as_list()[3]],
                                              kernel_size=(4, 4), stride=(2, 2), l2_strength=self.encoder.wd)
             self._debug(self.upscale5)
+            self.expand5 = conv2d('expand5', x=self.upscale5, batchnorm_enabled=True,
+                                      num_filters=self.encoder.conv1_1.shape.as_list()[3], kernel_size=(1, 1),
+                                      l2_strength=self.encoder.wd)
+            self._debug(self.expand5)
 
         with tf.name_scope('final_score'):
-            self.fscore = conv2d('fscore', x=self.upscale5, batchnorm_enabled=True,
+            self.fscore = conv2d('fscore', x=self.expand5, batchnorm_enabled=True,
                                       num_filters=self.params.num_classes, kernel_size=(1, 1),
                                       l2_strength=self.encoder.wd)
             self._debug(self.fscore)
