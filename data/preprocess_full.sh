85544b60af66a186298b6dec58da7455c4bd633d
Moemen AbdelRazek <runmoemen@gmail.com>
2017-09-26 09:13:34 -0600

    added test config for unet + modified preprocess cityscapes to generate two crops instead of resize

--- data/preprocess_full.sh
+++ data/preprocess_full.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python preprocess_cityscapes.py --root=/home/eren/Data/Cityscapes/ --rescale=0.5 --output_file=train.h5 --mode=normal --bs=5
+python preprocess_cityscapes.py --root=../../cityscapes/ --crop=0.5 --rescale=0.5 --output_file=train.h5 --mode=normal --bs=5
