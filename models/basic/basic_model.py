5696d8e7f33a7b1a80f51d0c10ae339043a0db5b
mennatullah siam <mennatul@ualberta.ca>
2017-09-28 14:46:37 -0600

    fix another error

--- models/basic/basic_model.py
+++ models/basic/basic_model.py
@@ -34,7 +34,7 @@ class BasicModel:
         self.params.img_height = self.args.img_height
         self.params.num_channels = self.args.num_channels
         self.params.num_classes = self.args.num_classes
-        self.params.class_weights= np.load(self.args.outdir+'weights.npy')
+        self.params.class_weights= np.load(self.args.data_dir+'weights.npy')
         self.params.weighted_loss= self.args.weighted_loss
         # Input
         self.x_pl = None
