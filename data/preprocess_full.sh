30c275d78cbe1cd7043fc2ca7b83fb08e30d1688
mennatullah siam <mennatul@ualberta.ca>
2017-09-28 14:47:30 -0600

    added weight computation to bash file processing the data

--- data/preprocess_full.sh
+++ data/preprocess_full.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 python preprocess_cityscapes.py --root=/home/eren/Data/Cityscapes/ --crop=0.5 --rescale=0.5 --output_file=train.h5 --mode=normal --bs=5
+python compute_weights full_cityscapes
