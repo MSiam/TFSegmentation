84fc7bd7b8bea07e1e3295dffb1b8943131dfaa3
Moemen <runmoemen@gmail.com>
2017-10-12 17:52:46 +0200

    preprocessing by h5 pending

--- data/preprocess_full.sh
+++ data/preprocess_full.sh
@@ -1,4 +1,6 @@
+#!/usr/bin/env bash
+
 #python preprocess_cityscapes.py --root=/home/eren/Data/Cityscapes/ --crop=0.5 --rescale=0.5 --output_file=train.h5 --mode=normal --bs=5
 #python preprocess_cityscapes.py --root=/home/eren/Data/Cityscapes/ --rescale=0.5 --output_file=train.h5 --mode=normal --bs=5
-python preprocess_cityscapes_tfrecords.py --root=../../Cityscapes/ --rescale=0.5 --dir=train_extra --out=cscapes_train.tfrecords
-
+python preprocess_cityscapes_tfrecords.py --root=../../Cityscapes/ --rescale=0.5 --dir=train_extra --out=cscapes_train.h5
+python preprocess_cityscapes_tfrecords.py --root=../../Cityscapes/ --rescale=0.5 --dir=val --out=cscapes_val.h5
