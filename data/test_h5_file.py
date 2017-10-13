38cfc958595d6ba38ce81b50d23a82631e208c3d
Moemen ABDELRAZEK <runmoemen@gmail.com>
2017-10-13 11:49:32 +0200

    testing the h5 file

--- data/test_h5_file.py
+++ data/test_h5_file.py
@@ -4,8 +4,8 @@ import numpy as np
 filename = 'cscapes_train.h5'
 f = h5py.File(filename, 'r')
 
-inputs = f['input'][0:100]
-outputs = f['output'][0:100]
+inputs = f['X'][0:100]
+outputs = f['Y'][0:100]
 
 print(inputs.shape)
 print(inputs.dtype)
