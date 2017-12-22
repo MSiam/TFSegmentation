831678c2f6a45eeb5bbfe072771347b06bc68114
moemen95 <runmoemen@gmail.com>
2017-12-22 18:54:31 +0200

    FPS Meter is integrated successfully waiting for testing it.

--- run.sh
+++ run.sh
@@ -60,6 +60,9 @@
 #3- UNet ResNet18 Train
 python3 main.py --load_config=unet_resnet18_train2.yaml train Train LinkNET
 
+# - UNet ResNet18 Inference .. FPS Test
+#python3 main.py --load_config=unet_resnet18_train2.yaml inference Train LinkNET
+
 #4- UNet ResNet18 Test
 #python3 main.py --load_config=unet_resnet18_test.yaml test Train LinkNET
 
