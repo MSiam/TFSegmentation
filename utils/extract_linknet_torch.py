41a657ba92c9064eb422330d993152d67e01633f
mennatullah siam <mennatul@ualberta.ca>
2017-12-27 21:02:53 -0700

    fixed loading weights into our model

--- utils/extract_linknet_torch.py
+++ utils/extract_linknet_torch.py
@@ -5,8 +5,9 @@ import torchfile
 from collections import OrderedDict
 import pdb
 
-model_weights= torchfile.load('/usr/data/menna/LinkNet-1.0/dict_net.t7')
+model_weights= torchfile.load('/home/eren/Work/TFSegmentation/pretrained_weights/dict_net.t7')
 
+pdb.set_trace()
 model_weights= OrderedDict(model_weights)
 
 # Transpose conv and fc weights
@@ -19,6 +20,5 @@ for k, v in model_weights.items():
         model_weights[k] = v
     print('name: ' + k + " - shape: " + str(model_weights[k].shape))
 
-pdb.set_trace()
 with open('linknet_weights.pkl', "wb") as f:
     pickle.dump(model_weights, f)
