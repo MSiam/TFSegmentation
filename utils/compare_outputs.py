d728df1468ae2e018e2ce76fa791c7f2933dbd4c
mennatullah siam <mennatul@ualberta.ca>
2017-12-28 19:00:28 -0700

    comparing torch vs tf linknet

--- utils/compare_outputs.py
+++ utils/compare_outputs.py
@@ -6,8 +6,9 @@ import os
 import torchfile
 import pickle
 import numpy as np
+import pdb
 
-# torch_output = torchfile.load('../out_networks_layers/dict_net.t7')
+torch_output = torchfile.load('../out_networks_layers/dict_out.t7')
 
 with open('../out_networks_layers/out_linknet_layers.pkl', 'rb') as ff:
     our_output = pickle.load(ff, encoding='latin1')
@@ -15,6 +16,10 @@ with open('../out_networks_layers/out_linknet_layers.pkl', 'rb') as ff:
 print(type(our_output))
 print(len(our_output))
 
+print(len(torch_output.items()))
+
 """
 Our output contains a list with all outputs MTRTBEN
 """
+pdb.set_trace()
+
