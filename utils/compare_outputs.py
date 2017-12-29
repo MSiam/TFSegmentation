33039e34d3ee57b2e9eb38a0c593732f865f1da1
moemen95 <runmoemen@gmail.com>
2017-12-29 03:37:41 +0200

    Get the output of all layers in a pickle (LIST)

--- utils/compare_outputs.py
+++ utils/compare_outputs.py
@@ -7,7 +7,14 @@ import torchfile
 import pickle
 import numpy as np
 
-torch_output = torchfile.load('../out_networks_layers/dict_net.t7')
+# torch_output = torchfile.load('../out_networks_layers/dict_net.t7')
 
 with open('../out_networks_layers/out_linknet_layers.pkl', 'rb') as ff:
     our_output = pickle.load(ff, encoding='latin1')
+
+print(type(our_output))
+print(len(our_output))
+
+"""
+Our output contains a list with all outputs MTRTBEN
+"""
