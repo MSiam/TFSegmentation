52d114ae45f698386400811e83151dadf50ecbe9
mennatullah siam <mennatul@ualberta.ca>
2017-12-29 18:46:35 -0700

    still debugging linknet problem in deconv

--- utils/extract_linknet_torch.py
+++ utils/extract_linknet_torch.py
@@ -6,19 +6,16 @@ from collections import OrderedDict
 import pdb
 
 model_weights= torchfile.load('/home/eren/Work/TFSegmentation/pretrained_weights/dict_net.t7')
-
 pdb.set_trace()
-model_weights= OrderedDict(model_weights)
-
 # Transpose conv and fc weights
+model_weights2= {}
 for k, v in model_weights.items():
     if len(v.shape) == 4:
-        model_weights[k] = np.transpose(v, (2, 3, 1, 0))
-    elif len(v.shape) == 2:
-        model_weights[k] = np.transpose(v)
+        model_weights2[k.decode("utf-8")] = np.transpose(v, (2, 3, 1, 0))
     else:
-        model_weights[k] = v
-    print('name: ' + k + " - shape: " + str(model_weights[k].shape))
+        model_weights2[k.decode("utf-8") ] = v
+    print('name: ' + str(k) + " - shape: " + str(model_weights2[k.decode("utf-8")].shape))
 
 with open('linknet_weights.pkl', "wb") as f:
-    pickle.dump(model_weights, f)
+    pickle.dump(model_weights2, f)
+
