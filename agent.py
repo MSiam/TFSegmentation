84329bc50587509eed9e8359bda7bdae77064866
Mennatullah Siam <menna@cedar5.cedar.computecanada.ca>
2018-01-02 16:38:35 -0800

    running on compcanada

--- agent.py
+++ agent.py
@@ -14,8 +14,8 @@ import pdb
 import pickle
 from utils.misc import calculate_flops
 
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
+#os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 
 class Agent:
