149e6e759acebd4a45ec8bb63a36c07578b4c39e
Mostafa Gamal <mostafa.gamal8086@gmail.com>
2018-05-01 17:40:55 +0200

    TF Supported is 1.7 for the Data Iterator. For older versions, change tensorflow.python.data to tensorflow.contrib.data

--- optimize.sh
+++ optimize.sh
@@ -5,6 +5,9 @@
 #Second Run freeze graph to convert all variables to constant operations
 python -m tensorflow.python.tools.freeze_graph --input_graph graph.pb --input_checkpoint experiments/coarsepre_fcn8s_shufflenet/checkpoints/best/-236744 --output_graph graph_frozen.pb --output_node_names=network/output/ArgMax
 # OR For TF 1.7 from source
+#If not built, build it from source.
+#bazel build tensorflow/python/tools:freeze_graph
+
 #bazel-bin/tensorflow/python/tools/freeze_graph --input_graph=/home/mg/Desktop/Repositories/TFSegmentation/graph.pb --input_checkpoint=/home/mg/Desktop/Repositories/TFSegmentation/experiments/coarsepre_fcn8s_shufflenet/checkpoints/best/-236744 --output_graph=/home/mg/Desktop/Repositories/TFSegmentation/graph_frozen.pb --output_node_names=network/output/ArgMax
 
 
